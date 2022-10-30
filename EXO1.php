307fb2e331cd2a5f590d7015b6a7033625ea1052
kodjo-anansi <116923227+kodjo-anansi@users.noreply.github.com>
2022-10-30 23:47:46 +0000

    Add files via upload

---
+++ EXO1.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Ecrire un sript qui génère  3 nombres entre -10 et 10 puis range ces nombres dans l'ordre croissant
+*/
+
+$x=rand(-10,10);
+$y=rand(-10,10);
+$z=rand(-10,10);
+$inter;
+
+$mini=$x;
+$maxi=$x;
+
+if($y<$mini){
+    $mini=$y;
+}
+if($y>$maxi){
+    $maxi=$y;
+}
+
+if($z<$mini){
+    $mini=$z;
+}
+if($z>$maxi){
+    $maxi=$z;
+}
+
+if(($x<=$y && $y<=$z) || ($z<=$y && $y<=$x)){
+    $inter=$y;
+}
+if(($y<=$z && $z<=$x) || ($x<=$z && $z<=$y)){
+    $inter=$z;
+}if(($z<=$x && $x<=$y) || ($y<=$x && $x<=$z)){
+    $inter=$x;
+}
+
+echo("x = $x");echo("<br>");
+echo("y = $y");echo("<br>");
+echo("z = $z");echo("<br>");
+echo("Ordre croissant : $mini , $inter , $maxi");echo("<br>");
+?>
---
+++ EXO4.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Ecrire un script qui génère N notes puis détermine et affiche:
+- la moyenne des notes
+- la note la plus grande 
+- la note la plus petite
+NB: N est aussi générer et est positif
+*/
+
+$N=rand(1,27);
+$moy;
+
+echo("N = $N");echo("<br>");echo("<br>");
+echo("Notes : ");echo("<br>");
+
+$note=rand(0,20);
+$mini=$note;
+$maxi=$note;
+$som=$note;
+echo("$note");echo("<br>");
+
+for ($i=1; $i<$N ; $i++){
+    $note=rand(0,20);
+    echo("$note");echo("<br>");
+
+    $som+=$note;
+    
+    if($note < $mini){
+        $mini=$note;
+    }
+    if($note > $maxi){
+        $maxi=$note;
+    }
+}
+
+$moy=$som/$N;
+
+echo("<br>");
+echo("La moyenne des notes est : $moy");echo("<br>");
+echo("La note la plus grande est : $maxi");echo("<br>");
+echo("La note la plus petite est : $mini");echo("<br>");
+?>
